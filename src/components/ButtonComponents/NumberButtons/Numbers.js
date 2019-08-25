136a9ee7a68b2970fd35923c65c8ef126dac85ff
Your name <a.m.verdine@gmail.com>
2019-08-25 18:41:42 -0400

    Update numbers.js by mapping over the numbers array and adding NumbersButton component

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -7,11 +7,15 @@ import { numbers } from './data.js'
 const Numbers = () => {
   // STEP 2 - add the imported data to state
   const [numberState, setNumberState] = useState(numbers)
+  const changeNumber = () => {
+    setNumberState(numberState)
+  }
   return (
-    <div>
-      {/* STEP 3 - Use .map() to iterate over your array data and return a button
-       component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+    <div>  { numberState.map((item, index) => {
+      return <NumberButton key={index} number={changeNumber}/>
+    })};
     </div>
-  );
+  )
 };
+
+export default Numbers;
