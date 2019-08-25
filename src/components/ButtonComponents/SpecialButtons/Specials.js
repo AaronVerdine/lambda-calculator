ded60d8eff84f278affdd11f086d1d277b4135d8
Your name <a.m.verdine@gmail.com>
2019-08-25 19:42:12 -0400

    Update specials.js by mapping over the specials array and adding SpecialButton component

--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -1,18 +1,27 @@
 import React, {useState } from "react";
 
 //import any components needed
-
+import SpecialButton from './SpecialButton'
 //Import your array data to from the provided data file
 import {specials} from './data'
 
-const Specials = () => {
+const Specials = (props) => {
+
   // STEP 2 - add the imported data to state
   const [specialState, setSpecialState] = useState(specials)
+  const changeSpecial = () => {
+      setSpecialState(specialState)
+  }
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+       {specialState.map((item, index) => {
+         return <SpecialButton key={index} number={changeNumber} />
+       })}
     </div>
   );
 };
+
+export default Specials;
